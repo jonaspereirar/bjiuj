df84333af353eb215ac1267967acb963e1402267
jonaspereirar <jonasprodrigues@gmail.com>
2024-01-19 10:26:47 +0000

    feat add: creat user-case register

---
+++ api/src/http/controllers/register.ts
@@ -0,0 +1,26 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { registerUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send
+}
