d17beedc1b8e06372b6d4964e657a4b2924f4100
jonaspereirar <jonasprodrigues@gmail.com>
2024-01-19 12:55:40 +0000

    feat add: Interface repository

--- api/src/http/controllers/register.ts
+++ api/src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
-import { InMemoryUsersRepository } from '@/repositories/in-memory-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,7 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new InMemoryUsersRepository()
+    const usersRepository = new PrismaUsersRepository()
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({
