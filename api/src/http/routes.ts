7f6e25402f78200c2cf9c800233f8cd19f8bc393
jonaspereirar <jonasprodrigues@gmail.com>
2024-01-21 22:14:46 +0000

    feat add: authentication flow with jwt token

--- api/src/http/routes.ts
+++ api/src/http/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from 'fastify'
 
 import { authenticate } from '@/http/controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
