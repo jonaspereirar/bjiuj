bf2a2d29d60e789ee0c5f0ce36937c8b86ad698a
jonaspereirar <jonasprodrigues@gmail.com>
2024-01-31 20:21:27 +0000

    feat add: prisma test environment

--- api/vite.config.ts
+++ api/vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    dir: 'src', // Essa linha
+  },
 })
